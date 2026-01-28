75b861c92b85d459d14a6dc5bf8195d5a1ec4306
Iqbal <singh.iqbal.2398@gmail.com>
2026-01-28 12:14:13 +0530

    Intital Commit

---
+++ src/main/java/com/CuriosityBox/CuriosityBox/CuriosityBoxApplication.java
@@ -0,0 +1,13 @@
+package com.CuriosityBox.CuriosityBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuriosityBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuriosityBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CuriosityBox/CuriosityBox/CuriosityBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CuriosityBox.CuriosityBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuriosityBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
