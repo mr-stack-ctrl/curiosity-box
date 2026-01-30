1bf7c63c021d1747daa2197fa8645c9e2516b3ac
Iqbal Singh <v18230308@gmail.com>
2026-01-30 17:42:56 +0530

    Update TestController.java

--- src/main/java/com/CuriosityBox/CuriosityBox/Controller/TestController.java
+++ src/main/java/com/CuriosityBox/CuriosityBox/Controller/TestController.java
@@ -3,7 +3,7 @@ package com.CuriosityBox.CuriosityBox.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 @RestController
 @RequestMapping("/")
 public class TestController {
