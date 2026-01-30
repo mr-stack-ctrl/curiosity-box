9dcb056311b694d515b60237342ceca4aa6dc163
Iqbal Singh <v18230308@gmail.com>
2026-01-30 17:33:49 +0530

    Update TestController.java

--- src/main/java/com/CuriosityBox/CuriosityBox/Controller/TestController.java
+++ src/main/java/com/CuriosityBox/CuriosityBox/Controller/TestController.java
@@ -10,6 +10,7 @@ public class TestController {
 
 
     // This is the testing cotroller
+     @CrossOrigin(origins = "*")
     @GetMapping(path = "/hi")
     public String hi(){
         return "Hi Guys";
